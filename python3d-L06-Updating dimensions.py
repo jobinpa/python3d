d770304fd527c2e4b69f81cf2074f7c5e17da5b0
Pascal Jobin <32720778+jobinpa@users.noreply.github.com>
2021-10-30 13:59:20 -0600

    Added lesson 06

---
+++ python3d-L06-Updating dimensions.py
@@ -0,0 +1,15 @@
+from vpython import *
+import numpy as np
+
+mySphere = sphere(radius=1, color=color.red, opacity=0.5)
+
+while True:
+    for myRadius in np.linspace(0.1, 1, 1000):
+        rate(250)
+        mySphere.radius = myRadius
+    
+    for myRadius in np.linspace(1, 0.1, 1000):
+        rate(250)
+        mySphere.radius = myRadius
+
+    pass
